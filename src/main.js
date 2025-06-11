9235825633091839751bde6089dce8351229973a
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-06-11 12:48:37 +0300

    main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './js/mobile-menu.js';
-import './js/artists-section.js';
+import { initArtistSection } from './js/artists-section.js'
 import './js/feedback-section.js';
 import './js/artist-details-modal.js';
 import './js/soundwave-api.js';
+
+initArtistSection();
